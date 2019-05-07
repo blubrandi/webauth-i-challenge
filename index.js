e20b69306c0f576d95d159891d0dffcae7d2233c
blubrandi <blubrandi0@gmail.com>
2019-05-07 08:16:02 -0600

    Added scripts to package.json, defined server in index.js, changed port number in index.js.

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const helmet = require('helmet')
 const cors = require('cors')
 const bcrypt = require('bcryptjs')
 
+const server = express()
+
 server.use(helmet())
 server.use(express.json())
 server.use(cors())
 
-const port = process.env.PORT || 5000
+server.get('/', (req, res) => {
+    res.send('We\'re up and running!')
+})
+
+const port = process.env.PORT || 4000
 server.listen(port, () => console.log(`\n *** Running on port ${port} *** \n`))
