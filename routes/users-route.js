3b03e031fdcf0091c62d50750d39c9ce2306ac85
blubrandi <blubrandi0@gmail.com>
2019-05-07 16:48:35 -0600

    Added users route

--- routes/users-route.js
+++ routes/users-route.js
@@ -1,7 +1,15 @@
 const express = require('express')
 
-const users = require('../users/users-model.js')
+const Users = require('../users/users-model.js')
 
 const router = express.Router()
 
-router.get('/')
+router.get('/', restricted, (req, res) => {
+    Users.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => res.send(err));
+});
+
+module.exports = router;
