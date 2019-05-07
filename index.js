7ba825de2f84eeb411affbbd1dcce05c87322a31
blubrandi <blubrandi0@gmail.com>
2019-05-07 07:37:35 -0600

    Added port info to index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+const bcrypt = require('bcryptjs')
+
+server.use(helmet())
+server.use(express.json())
+server.use(cors())
+
+const port = process.env.PORT || 5000
+server.listen(port, () => console.log(`\n *** Running on port ${port} *** \n`))
