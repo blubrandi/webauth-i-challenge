7228172b3e82687ccd089ff982d7b115dfa19012
blubrandi <blubrandi0@gmail.com>
2019-05-08 10:16:29 -0600

    Fixed path for route causing error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express')
-
+// const express = require('express') 
+console.log('here')
 const server = require('./api/server.js')
 
 
--- routes/users-route.js
+++ routes/users-route.js
@@ -1,8 +1,7 @@
-const express = require('express')
+const router = require('express').Router()
 
 const Users = require('../users/users-model.js')
-
-const router = express.Router()
+const restricted = require('../auth/restricted.js')
 
 router.get('/', restricted, (req, res) => {
     Users.find()
@@ -10,6 +9,6 @@ router.get('/', restricted, (req, res) => {
             res.json(users);
         })
         .catch(err => res.send(err));
-});
+})
 
 module.exports = router;
